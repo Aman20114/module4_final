dee77fc8b7e4b2ba5851c81f86a52c20360f5705
Aman Tiwari <110708815+Aman20114@users.noreply.github.com>
2023-07-04 23:23:04 +0530

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import {applyMiddleware } from 'redux';
+import {createStore} from 'redux'
+import thunk from 'redux-thunk';
+import { combineReducers } from 'redux';
+import postsReducer from './Reducers/postsReducer';
+import detailsReducer from './Reducers/detailsReducer';
+
+const rootReducer = combineReducers({
+    posts:postsReducer,
+    Ipost: detailsReducer,
+})
+
+const store = createStore(rootReducer,applyMiddleware(thunk))
+
+export default store;
